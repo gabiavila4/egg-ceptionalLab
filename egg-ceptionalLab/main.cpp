d4da793b2ba6c261069226bfc2306aaa959b61b3
gabiavila4 <123492198+gabiavila4@users.noreply.github.com>
2025-10-14 10:06:05 -0500

    Initial Commit

---
+++ egg-ceptionalLab/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  egg-ceptionalLab
+//
+//  Created by Gabriela Avila on 10/14/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
